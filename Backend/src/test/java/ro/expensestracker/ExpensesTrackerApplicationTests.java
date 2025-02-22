64b07473f6130210aacef1eff8652c7da148139f
Dumitrascu Luca <dumitrasculuca07@gmail.com>
2025-02-22 14:39:57 +0200

    Added Frontend and Backend directories

---
+++ Backend/src/test/java/ro/expensestracker/ExpensesTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.expensestracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
